f510cf36c15a6a4412ebe77e33db645f973ac777
Uchirai <uchirai.govere@gmail.com>
2023-05-05 10:33:44 +0200

    second

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+const path = require("path")
 const express = require("express")
 const colors = require("colors")
 const dotenv = require("dotenv").config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
 
 const PORT = process.env.PORT || 8000
 
@@ -11,14 +12,24 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.status(201).send({message: "Welcome to the support desk API!"})
-})
+app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/tickets", require("./routes/ticketRoutes"))
 
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/tickets', require('./routes/ticketRoutes'))
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.status(201).send({ message: "Welcome to the support desk API!" })
+  })
+}
 
 app.use(errorHandler)
 
